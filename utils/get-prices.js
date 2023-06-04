cb7d3411cd3111b4c1621016894448849640cd09
Anders Brownworth <anders-git@evantide.com>
2023-06-04 23:12:42 +0000

    get-prices.js: start from the timestamp of the last price recorded

--- utils/get-prices.js
+++ utils/get-prices.js
@@ -15,10 +15,17 @@ const sub = require('date-fns/sub');
 
     await client.connect();
 
-    const end = new Date().getTime();
-    const start = sub(end, {days: 1});
-
     let res;
+    let start;
+    const end = new Date().getTime();
+
+    // get date of last price
+    res = await client.query('SELECT stamp FROM prices ORDER BY stamp DESC LIMIT 1');
+    if (res.rows.length == 1) // shapella (withdrawal unlock) was at epoch 194048 slot 6209536
+	start = new Date(res.rows[0].stamp).getTime();
+    else
+	start = sub(end, {days: 1});
+
     res = await axios.get('https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from=' + (start / 1000) + '&to=' + (end / 1000));
 
     if (res.data && res.data.prices) {
