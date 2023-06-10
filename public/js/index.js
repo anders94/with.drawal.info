6c352b0e943dbb163e40cdeb24e73924342fb7ff
Anders Brownworth <anders-git@evantide.com>
2023-06-10 17:41:07 +0000

    numberFormat

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,8 @@
 $(document).ready(() => {
-    // stuff here
+    const usdFormatter = new Intl.NumberFormat('en-US', {
+	style: 'currency',
+	currency: 'USD',
+	//minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+	maximumFractionDigits: 0
+    });
 });
