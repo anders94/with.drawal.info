8ab94609c858985fd3ec90054800543c8c93633a
Anders Brownworth <anders-git@evantide.com>
2023-06-18 03:37:33 +0000

    add search

--- routes/index.js
+++ routes/index.js
@@ -98,6 +98,17 @@ module.exports = {
 	finally {
 	    return next;
 	}
+    },
+    search: async (req, res, next) => {
+	const { query } = req.body;
+
+	if (query && !isNaN(query) && !isNaN(parseInt(query)))
+	    res.redirect('/validator/' + query);
+	else
+	    res.render('error', {message: 'not a valid number!'});
+
+	return next;
+
     }
 
 };
