4f650a5e90326771c927e60bf73383028e773596
Anders Brownworth <anders-git@evantide.com>
2023-06-28 23:43:34 +0000

    add express-session

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
 const datefns = require('date-fns');
 const routes = require('./routes');
 
@@ -14,6 +16,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 const http = require('http').createServer(app);
 
 (async () => {
+    app.use(session({
+	cookie: { maxAge: 10 * 86400000 },
+	store: new MemoryStore({ checkPeriod: 86400000 }),
+	resave: false,
+	saveUninitialized: false,
+	secret: process.env.SESSIONSECRET || 'd46e4e10115103fa32780d259d0705e4'
+    }));
+
     app.use((req, res, next) => {
 	res.locals.datefns = datefns;
 	next();
