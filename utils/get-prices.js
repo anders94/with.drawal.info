8781e384521e5041637c950f0323c1b270d56123
Anders Brownworth <anders-git@evantide.com>
2023-07-08 07:16:00 -0400

    get-prices.js: wrap in a try / catch

--- utils/get-prices.js
+++ utils/get-prices.js
@@ -11,45 +11,61 @@ const sub = require('date-fns/sub');
 	password: process.env.DB_PASS,
     });
 
-    console.log('start', new Date());
+    try {
+	console.log('start', new Date());
 
-    await client.connect();
+	await client.connect();
 
-    let res;
-    let start;
-    const end = new Date().getTime();
+	await client.query('BEGIN');
 
-    // get date of last price
-    res = await client.query('SELECT stamp FROM prices ORDER BY stamp DESC LIMIT 1');
-    if (res.rows.length == 1) // shapella (withdrawal unlock) was at epoch 194048 slot 6209536
-	start = new Date(res.rows[0].stamp).getTime();
-    else
-	start = sub(end, {days: 1});
+	let res;
+	let start;
+	const end = new Date().getTime();
 
-    res = await axios.get('https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from=' + (start / 1000) + '&to=' + (end / 1000));
+	// get date of last price
+	res = await client.query('SELECT stamp FROM prices ORDER BY stamp DESC LIMIT 1');
+	if (res.rows.length == 1) // shapella (withdrawal unlock) was at epoch 194048 slot 6209536
+	    start = new Date(res.rows[0].stamp).getTime();
+	else
+	    start = sub(end, {days: 1});
 
-    if (res.data && res.data.prices) {
-	const prices = res.data.prices;
-	for (const price of prices) {
-	    console.log(' ', new Date(price[0]), price[1]);
-	    res = await client.query('SELECT price, stamp FROM prices WHERE stamp = $1', [new Date(price[0])]);
-	    if (res.rows.length == 0)
-		await client.query('INSERT INTO prices (stamp, price) VALUES ($1, $2)', [new Date(price[0]), price[1]]);
-	    else
-		console.log('  already have a price for', res.rows[0].stamp);
+	// remove all pricings on slots newer than the newest price we have
+	client.query('UPDATE slots SET price_id = NULL WHERE stamp > $1', [start]);
+
+	res = await axios.get('https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from=' + (start / 1000) + '&to=' + (end / 1000));
+
+	if (res.data && res.data.prices) {
+	    const prices = res.data.prices;
+	    for (const price of prices) {
+		console.log(' ', new Date(price[0]), price[1]);
+		res = await client.query('SELECT price, stamp FROM prices WHERE stamp = $1', [new Date(price[0])]);
+		if (res.rows.length == 0)
+		    await client.query('INSERT INTO prices (stamp, price) VALUES ($1, $2)', [new Date(price[0]), price[1]]);
+		else
+		    console.log('  already have a price for', res.rows[0].stamp);
+
+	    }
 
 	}
 
+	console.log('updating slots that don\'t yet have prices');
+	await client.query(
+	    `UPDATE slots s
+               SET price_id = (SELECT id FROM prices ORDER BY ABS(EXTRACT(EPOCH FROM AGE(stamp, s.stamp))) LIMIT 1)
+             WHERE s.price_id IS NULL;`
+	);
+
+	client.query('COMMIT');
+
     }
+    catch (e) {
+	client.query('ROLLBACK');
+	console.log(e);
 
-    console.log('updating withdrawals that don\'t yet have prices');
-    await client.query(
-	`UPDATE slots s
-           SET price_id = (SELECT id FROM prices ORDER BY ABS(EXTRACT(EPOCH FROM AGE(stamp, s.stamp))) LIMIT 1)
-         WHERE s.price_id IS NULL;`
-    );
+    }
+    finally {
+	console.log('end', new Date());
 
-    console.log('end', new Date());
-
-    await client.end();
+	await client.end();
+    }
 })();
