512019c79abe2fd61ea6e78f8cce8d90fae108c3
Anders Brownworth <anders-git@evantide.com>
2023-07-11 23:19:17 +0000

    /address/0xabc123: limit to 1000 addresses and speed up price query

--- routes/address/index.js
+++ routes/address/index.js
@@ -11,17 +11,19 @@ module.exports = {
 	    const w = await db.query(
 		`SELECT
                    s.stamp, w.id, w.slot_id, w.address, w.amount / 1000000000.0 AS amount,
-                   SUM((SELECT price FROM prices ORDER BY ABS(EXTRACT(EPOCH FROM AGE(stamp, s.stamp))) LIMIT 1) * (w.amount / 1000000000.0)) AS usd_value
+                   SUM(COALESCE(p.price, (SELECT price FROM prices ORDER BY ABS(EXTRACT(EPOCH FROM AGE(stamp, s.stamp))) LIMIT 1)) * (w.amount / 1000000000.0)) AS usd_value
                  FROM
                    withdrawals w
                      LEFT JOIN slots s ON w.slot_id = s.id
+                     LEFT JOIN prices p ON s.price_id = p.id
                      LEFT JOIN validators v ON w.validator_id = v.id
                  WHERE
                    w.address = $1
                  GROUP BY
                    s.stamp, v.id, w.id, w.slot_id, w.address, w.amount
                  ORDER BY
-                    w.slot_id DESC;`, [address]
+                    w.slot_id DESC
+                 LIMIT 1000`, [address]
 	    );
 	    res.render('address', {
 		address: address,
