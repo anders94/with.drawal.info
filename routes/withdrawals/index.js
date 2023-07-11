84440c75234ac537b83aa3a553e91c1a46962164
Anders Brownworth <anders-git@evantide.com>
2023-07-11 23:21:49 +0000

    limit withdrawals to 2500, homepage chart to log scale

--- routes/withdrawals/index.js
+++ routes/withdrawals/index.js
@@ -6,19 +6,17 @@ module.exports = {
 	    const w = await db.query(
 		`SELECT
                    s.stamp, w.id, v.id AS validator_id, w.slot_id, w.address, w.amount / 1000000000.0 AS amount,
-                   SUM((SELECT price FROM prices ORDER BY ABS(EXTRACT(EPOCH FROM AGE(stamp, s.stamp))) LIMIT 1) * (w.amount / 1000000000.0)) AS usd_value
+                   SUM(COALESCE(p.price, (SELECT price FROM prices ORDER BY ABS(EXTRACT(EPOCH FROM AGE(stamp, s.stamp))) LIMIT 1)) * (w.amount / 1000000000.0)) AS usd_value
                  FROM
                    withdrawals w
                      LEFT JOIN slots s ON w.slot_id = s.id
                      LEFT JOIN validators v ON w.validator_id = v.id
-                     LEFT JOIN users2validators u2v ON v.id = u2v.validator_id
-                 WHERE u2v.user_id = $1
+                     LEFT JOIN prices p ON s.price_id = p.id
                  GROUP BY
                    s.stamp, v.id, w.id, w.slot_id, w.address, w.amount
                  ORDER BY
                     w.slot_id DESC
-                 LIMIT 500;`,
-		['d3381029-a22f-4c5f-85aa-6b80bdcadb4f']
+                 LIMIT 2500`
 	    );
 	    res.render('withdrawals', {
 		withdrawals: w.rows,
@@ -44,21 +42,21 @@ module.exports = {
 	    const w = await db.query(
 		`SELECT
                    s.stamp, w.id, v.id AS validator_id, w.slot_id, w.address, w.amount / 1000000000.0 AS amount,
-                   SUM((SELECT price FROM prices ORDER BY ABS(EXTRACT(EPOCH FROM AGE(stamp, s.stamp))) LIMIT 1) * (w.amount / 1000000000.0)) AS usd_value
+                   SUM(COALESCE(p.price, (SELECT price FROM prices ORDER BY ABS(EXTRACT(EPOCH FROM AGE(stamp, s.stamp))) LIMIT 1)) * (w.amount / 1000000000.0)) AS usd_value
                  FROM
                    withdrawals w
                      LEFT JOIN slots s ON w.slot_id = s.id
                      LEFT JOIN validators v ON w.validator_id = v.id
-                     LEFT JOIN users2validators u2v ON v.id = u2v.validator_id
+                     LEFT JOIN prices p ON s.price_id = p.id
                  WHERE
                    s.stamp >= $1 AND
-                   s.stamp <= $2 AND
-                   u2v.user_id = $3
+                   s.stamp <= $2
                  GROUP BY
                    s.stamp, v.id, w.id, w.slot_id, w.address, w.amount
                  ORDER BY
-                    w.slot_id DESC;`,
-		[start, end, 'd3381029-a22f-4c5f-85aa-6b80bdcadb4f']
+                    w.slot_id ASC
+                 LIMIT 2500`,
+		[start, end]
 	    );
 	    res.render('withdrawals', {
 		withdrawals: w.rows,
